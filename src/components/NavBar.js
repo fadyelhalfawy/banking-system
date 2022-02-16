bef79bd89e6ee8905b441cf29f6fac2a29224a40
Fady ElHalfawy <fadyhalfawy@gmail.com>
2022-02-16 13:24:56 +0200

    Add navbar component.

---
+++ src/components/NavBar.js
@@ -0,0 +1,31 @@
+import React from "react";
+import {Link} from "react-router-dom";
+import {NavLink} from "react-router-dom";
+
+const NavBar = () => {
+
+    return(
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <Link className="navbar-brand" to="/">Bank System...</Link>
+            <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+                <div className="navbar-nav">
+                    <NavLink className="nav-item nav-link" to="/home">
+                        Home
+                    </NavLink>
+                    <NavLink className="nav-item nav-link" to="/clients">
+                        Clients
+                    </NavLink>
+                    <NavLink className="nav-item nav-link" to="/transactions">
+                        Transactions
+                    </NavLink>
+                    <NavLink className="nav-item nav-link" to="/about-us">
+                        About Us
+                    </NavLink>
+                </div>
+
+            </div>
+        </nav>
+    );
+}
+
+export default NavBar;
