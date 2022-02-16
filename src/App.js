6a699f0591a95d541d18ec4ceb56fd5a25a204e2
Fady ElHalfawy <fadyhalfawy@gmail.com>
2022-02-16 13:30:47 +0200

    Update App.js by adding the switch component and route component.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,34 @@
+import React from "react";
+import NavBar from "./components/NavBar";
+import {Redirect, Switch, Route} from "react-router-dom";
+import {Home} from "./routers/Home";
+import Clients from "./routers/Clients";
+import {Transactions} from "./routers/Transactions";
+import {AboutUs} from "./routers/AboutUs";
+import {NotFound} from "./routers/NotFound";
+
 const App = () => {
-    return null;
+    return (
+        <React.Fragment>
+
+            <NavBar />
+
+            <main className={"container"}>
+
+                <Switch>
+                    <Route path={"/home"} component={Home} />
+                    <Route path={"/clients"} component={Clients} />
+                    <Route path={"/transactions"} component={Transactions} />
+                    <Route path={"/about-us"} component={AboutUs} />
+                    <Route path={"/notfound"} to={NotFound} />
+                    <Redirect from={"/"} exact to={"/home"} />
+                    <Redirect to={"/notfound"} />
+                </Switch>
+
+            </main>
+
+        </React.Fragment>
+    );
 }
 
 export default App;
