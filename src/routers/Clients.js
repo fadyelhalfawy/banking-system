daed69ff5adb488ab7322474a4c34a0e23af8fe2
Fady ElHalfawy <fadyhalfawy@gmail.com>
2022-02-19 06:55:39 +0200

    Add Clients.js

--- src/routers/Clients.js
+++ src/routers/Clients.js
@@ -1,9 +1,13 @@
 import {Component} from "react";
+import Joi from "joi-browser";
+import classes from "../modules/about-us.module.css";
 import SearchBoxForm from "../components/SearchBox";
 import ClientTable from "../clientTable/ClientTable";
 import Pagination from "../components/Pagination";
 import {getPageData} from "../helperFunctions/GetPageData";
 import {getClients} from "../service/ClientsService";
+import {SelectGenderForm} from "../components/SelectGenderForm";
+import InputForm from "../components/InputForm";
 
 export default class Clients extends Component {
     state = {
@@ -11,18 +15,25 @@ export default class Clients extends Component {
         searchQuery: "",
         pageSize: 5,
         currentPage: 1,
-        sortColumn: { path: "name", order: 'asc'}
+        sortColumn: {path: "name", order: 'asc'},
+        data: {money: ""},
+        errors: {}
+    };
+
+    schema = {
+        money: Joi.number().required().label("Money")
     };
 
     async componentDidMount() {
         const {data: clients} = await getClients();
-        this.setState({ clients });
+        this.setState({clients});
     };
 
     render() {
-        const {clients, searchQuery, currentPage, pageSize, sortColumn} = this.state;
-        const { length, clientPaginate } = getPageData(clients, pageSize, currentPage, sortColumn, searchQuery);
-        return(
+        const {clients, searchQuery, currentPage, pageSize, sortColumn, data, errors} = this.state;
+        const {length, clientPaginate} = getPageData(clients, pageSize, currentPage, sortColumn, searchQuery);
+        const {history} = this.props;
+        return (
             <div>
                 <SearchBoxForm
                     value={searchQuery}
@@ -40,20 +51,83 @@ export default class Clients extends Component {
                     currentPage={currentPage}
                     onPageChange={this.handlePageChange}
                 />
+
+                <SelectGenderForm
+                    id={"Client-1"}
+                    label={"Sender"}
+                    clients={clientPaginate}
+                />
+
+                <SelectGenderForm
+                    id={"Client-2"}
+                    label={"Receiver"}
+                    clients={clientPaginate}
+                />
+
+                <InputForm
+                    name={"money"}
+                    type={"text"}
+                    value={data["money"]}
+                    onChange={this.handleChange}
+                    label={<label className={classes.select}>Money</label>}
+                    placeholder={"Write the amount"}
+                    error={errors["money"]}
+                />
+
+                <button
+                    className={"btn btn-outline-success btn-space"}
+                    disabled={this.validate()}
+                    onClick={() => this.handleClickButton(history, "/transactions")}>
+                    {"Send Money"}
+                </button>
+
             </div>
         )
     }
 
-    handlePageChange = page => this.setState({currentPage: page} );
+    handlePageChange = page => this.setState({currentPage: page});
 
-    handleSearch = query => this.setState({ searchQuery: query, currentPage: 1 });
+    handleSearch = query => this.setState({searchQuery: query, currentPage: 1});
 
     handleSort = path => {
-      const sortColumn = { ...this.state.sortColumn };
-      if (sortColumn.path === path)
-          sortColumn.order = (sortColumn.order === 'asc') ? 'desc' : 'asc';
+        const sortColumn = {...this.state.sortColumn};
+        if (sortColumn.path === path)
+            sortColumn.order = (sortColumn.order === 'asc') ? 'desc' : 'asc';
 
-      this.setState({ sortColumn });
+        this.setState({sortColumn});
     };
 
-};
+    validate() {
+        const options = {abortEarly: false};
+        const {error} = Joi.validate(this.state.data, this.schema, options);
+
+        if (!error) return null;
+
+        const errors = {};
+
+        for (let item of error.details) errors[item.path[0]] = item.message;
+
+        return errors;
+    };
+
+    validateProperty = ({name, value}) => {
+        const obj = {[name]: value};
+        const schema = {[name]: this.schema[name]};
+        const {error} = Joi.validate(obj, schema);
+        return error ? error.details[0].message : null
+    };
+
+    handleChange = ({currentTarget: target}) => {
+        const {data, errors} = this.state;
+        const getErrors = {...errors};
+        const errorMessage = this.validateProperty(target);
+        if (errorMessage) getErrors[target.name] = errorMessage;
+        else delete getErrors[target.name];
+
+        const getData = {...data};
+        getData[target.name] = target.value;
+        this.setState({data: getData, errors: getErrors});
+    };
+
+    handleClickButton = (history, path) => history.push(path);
+}
