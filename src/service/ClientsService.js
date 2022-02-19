3386b1c1a3e205b4fbd5586af452e44a3c0b4d35
Fady ElHalfawy <fadyhalfawy@gmail.com>
2022-02-19 06:56:44 +0200

    Update Services file backend.

--- src/service/ClientsService.js
+++ src/service/ClientsService.js
@@ -1,10 +1,6 @@
 import http from "./HttpService";
 import config from "../config/config.json";
 
-// export const getClients = () => http.get(config.clientsPath);
-
-export function getClients() {
-    return http.get(config.clientsPath);
-};
+export const getClients = () => http.get(config.clientsPath);
 
 export const getClient = clientId => http.get(config.clientsPath + "/" + clientId);
