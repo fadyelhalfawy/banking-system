40bcfc305fd65ad9b62634feb3e008db405ab96a
Fady ElHalfawy <fadyhalfawy@gmail.com>
2022-02-22 06:07:04 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import NavBar from "./components/NavBar";
 import {Redirect, Switch, Route} from "react-router-dom";
 import {Home} from "./routers/Home";
 import Clients from "./routers/Clients";
-import {Transactions} from "./routers/Transactions";
+import Transactions from "./routers/Transactions";
 import {AboutUs} from "./routers/AboutUs";
 import {NotFound} from "./routers/NotFound";
 
