f627e57ed298c5ae938e2ff755200d34793adcdc
Fady ElHalfawy <fadyhalfawy@gmail.com>
2022-02-22 06:06:55 +0200

    Update FilteredData.js

--- src/helperFunctions/FilteredClients.js
+++
@@ -1,7 +0,0 @@
-export let filteredClients = (allClients, searchQuery) => {
-    let clients = allClients;
-
-    if (searchQuery) clients = allClients.filter(m => m.name.toLowerCase().startsWith(searchQuery.toLowerCase()));
-
-    return clients;
-};
