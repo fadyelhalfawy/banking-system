374b84e710820591dba479a9cc83e99025afab84
Fady ElHalfawy <fadyhalfawy@gmail.com>
2022-02-22 06:09:06 +0200

    Delete unused lines.

--- src/service/ClientsService.js
+++ src/service/ClientsService.js
@@ -2,5 +2,3 @@ import http from "./HttpService";
 import config from "../config/config.json";
 
 export const getClients = () => http.get(config.clientsPath);
-
-export const getClient = clientId => http.get(config.clientsPath + "/" + clientId);
