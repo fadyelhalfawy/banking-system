5e813f5157ae687e5712929d6a1eba65e316ece4
Fady ElHalfawy <fadyhalfawy@gmail.com>
2022-02-22 06:08:22 +0200

    Update SelectedForm to be reusable.

--- src/components/SelectGenderForm.js
+++ src/components/SelectGenderForm.js
@@ -1,17 +1,18 @@
 import React from "react";
 import classes from "../modules/about-us.module.css";
 
-export const SelectGenderForm = ({ id, label, clients }) => {
+export const SelectGenderForm = ({ id, label, clients, value, onChange }) => {
     return(
         <div className="form-group">
             <label className={`form-choose-label ${classes.select}`}
                    htmlFor={ id }>{label}</label>
 
-            <select className="custom-select" required>
+            <select value={value} onChange={onChange} className="custom-select" required>
+                <option defaultValue={"selected"}>Choose Client</option>
                 {clients.map(client =>
                     <option
-                        key={client.id}
-                        value={client.name}>
+                        key={client._id}
+                    >
                         {client.name}
                     </option>
                 )}
